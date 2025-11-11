7d62fd9782ff7240d63c0ef22f30578710334642
ragu812 <raguneshkumar812@gmail.com>
2025-11-11 16:27:02 +0530

    .

--- src/main.rs
+++ src/main.rs
@@ -1308,7 +1308,6 @@ fn main() {
         .contrast(0.2);
 
     // Load dataset with augmentation
-    let dataset = Image::directory(r"C:\Users\M.KUMAR\Downloads\diffusionModel\Pictures", 64, 64)
     let dataset = Image::directory(r"/home/raghunesh/Downloads/my_burn/Pictures", 64, 64)
         .unwrap()
         .with_augmentation(augmentation);
